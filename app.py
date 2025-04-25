c0a5e213bb5cee0532bd6db73c24f22f278d614a
Fengyuan12-science <constanta1584@outlook.com>
2025-04-25 16:27:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
     now = datetime.now()
     return render_template('index.html', now=now)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
+
